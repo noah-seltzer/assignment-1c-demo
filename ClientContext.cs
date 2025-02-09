c7d5b5a85f51ffc86c3513308449a1e41ab6ad19
Noah Seltzer <noahseltzer@outlook.com>
2025-02-08 22:13:11 -0800

    data(database):update connection string

--- ClientContext.cs
+++ ClientContext.cs
@@ -9,7 +9,7 @@ namespace MVCSampleApp
         public ClientContext() { }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server= DESKTOP-UEBA1R1;Database= lab3Test;Integrated Security= true; TrustServerCertificate=True");
+        => optionsBuilder.UseSqlServer();
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
